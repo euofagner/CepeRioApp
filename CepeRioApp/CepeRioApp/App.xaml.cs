78fd81afbdf098279812150cd7aa2bbc888f712c
Fagner <fagnergeo0@gmail.com>
2025-08-21 20:51:24 -0300

    Uploading project to GitHub

---
+++ CepeRioApp/CepeRioApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CepeRioApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CepeRioApp/CepeRioApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CepeRioApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
