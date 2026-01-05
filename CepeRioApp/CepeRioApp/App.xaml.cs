75cfc86d5ae7be858d7e81d39edb17c142d313d8
Fagner <fagnergeo0@gmail.com>
2026-01-05 10:24:04 -0300

    Added main buttons in home page

--- CepeRioApp/CepeRioApp/App.xaml.cs
+++ CepeRioApp/CepeRioApp/App.xaml.cs
@@ -25,6 +25,6 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new AppShell());
+        return new Window(_appShell);
     }
 }
